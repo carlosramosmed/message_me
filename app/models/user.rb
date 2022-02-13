bdd28157fbe95f1d4a68ab52430a19748c17d0d9
Carlos Ramos <carlosr9m@gmail.com>
2022-02-13 18:39:22 +0100

    create user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :username, 
+    presence: true, 
+    uniqueness: true, 
+    length: { 
+      in: 3..15, 
+      too_short: "Must be minimum 3 characters long", 
+      too_long: "Must be maximum 15 characters long" 
+    } 
+  has_secure_password
+end
