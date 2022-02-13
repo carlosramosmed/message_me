68dd117bce8f152a5471a85a3525e74b9fe2a325
Carlos Ramos <carlosr9m@gmail.com>
2022-02-13 16:49:19 +0100

    install Semantic UI dependency

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import '@doabit/semantic-ui-sass'
+import '@doabit/semantic-ui-sass/src/scss/semantic-ui.scss'
 import "channels"
 
 Rails.start()
