0251f9fbf3cb82a73c8e19043d2d104f098d7a35
Carlos Ramos <carlosr9m@gmail.com>
2022-02-13 18:44:08 +0100

    create messages model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
       in: 3..15, 
       too_short: "Must be minimum 3 characters long", 
       too_long: "Must be maximum 15 characters long" 
-    } 
+    }
+  has_many :messages
   has_secure_password
 end
